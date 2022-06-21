22febe9bd9ab93464a8b7d158bb3cbb2a3429a24
QAsper108 <semidog@gmail.com>
2022-06-21 16:12:49 +0300

    solve task 34

--- Program.cs
+++ Program.cs
@@ -4,3 +4,36 @@
 массиве.
 [345, 897, 568, 234] -> 2 */
 
+
+void Print(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+
+    }
+    Console.WriteLine();
+}
+
+void CheckDigit(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            count ++;
+        }
+    }
+    System.Console.WriteLine(count);
+}
+
+
+int[] mass = new int[6];
+for (int i = 0; i < mass.Length; i++)
+{
+    mass[i] = new Random().Next(100, 1000);
+}
+
+Print(mass);
+CheckDigit(mass);
